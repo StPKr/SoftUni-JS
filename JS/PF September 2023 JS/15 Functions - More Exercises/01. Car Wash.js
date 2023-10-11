b448fb680152e3dbc5d53f6f31878a2e260526f5
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-11 10:38:10 +0300

    Daily update

---
+++ JS/PF September 2023 JS/15 Functions - More Exercises/01. Car Wash.js
@@ -0,0 +1,14 @@
+function wash(arr) {
+    let value = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let command = arr[i];
+        switch (command) {
+            case "soap": value += 10; break;
+            case "water": value *= 1.2; break;
+            case "vacuum cleaner": value *= 1.25; break;
+            case "mud": value *= 0.9; break;
+        }
+    }
+    console.log(`The car is ${value.toFixed(2)}% clean.`);
+}
+wash(['soap', 'soap', 'vacuum cleaner', 'mud', 'soap', 'water'])
