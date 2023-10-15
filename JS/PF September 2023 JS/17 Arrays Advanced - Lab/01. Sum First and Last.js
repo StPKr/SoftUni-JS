0c0236029c4dd464f62195618d2b48b7b4670104
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-15 22:06:05 +0300

    Daily commit

--- JS/PF September 2023 JS/17 Arrays Advanced - Lab/01. Sum First and Last.js
+++ JS/PF September 2023 JS/17 Arrays Advanced - Lab/01. Sum First and Last.js
@@ -1,6 +1,7 @@
 function solve(arr) {
     // arr = arr.map(Number); - makes each element of the array into a Number;
     console.log(Number(arr[0]) + Number(arr[arr.length - 1]));
+    // console.log(Number(arr.shift()) + Number(arr.pop())); - alternative but better
                                     // + Number (arr.pop()); - alternative
 }
 solve(['20', '30', '40'])
