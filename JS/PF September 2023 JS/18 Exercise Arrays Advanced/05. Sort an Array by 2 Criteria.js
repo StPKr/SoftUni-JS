b8203baf79487d644da222566c1c13bbbd106285
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-16 12:21:11 +0300

    Update 05. Sort an Array by 2 Criteria.js

--- JS/PF September 2023 JS/18 Exercise Arrays Advanced/05. Sort an Array by 2 Criteria.js
+++ JS/PF September 2023 JS/18 Exercise Arrays Advanced/05. Sort an Array by 2 Criteria.js
@@ -1,6 +1,6 @@
 function solve(arr) {
-    arr.sort();
-    arr.sort((a, b) => a.length - b.length);
+    // arr.sort();
+    arr.sort((a, b) => a.length - b.length || a.localeCompare(b));
     console.log(arr.join("\n"))
 }
 solve(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);
