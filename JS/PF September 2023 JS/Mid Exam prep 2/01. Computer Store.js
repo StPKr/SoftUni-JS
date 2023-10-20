6cec65bcff9c98d44ebcc782556c09e9712fafde
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-20 22:03:18 +0300

    Mid exam prep 2 Task 1

--- JS/PF September 2023 JS/Mid Exam prep 2/01. Computer Store.js
+++ JS/PF September 2023 JS/Mid Exam prep 2/01. Computer Store.js
@@ -1,11 +1,28 @@
 function solve(inputArray) {
-let customerType = inputArray.pop();
-console.log(customerType);
-inputArray.map(Number);
-if (inputArray.includes(x => x < 0)){
-    
-}
-
+    let customerType = inputArray.pop();
+    inputArray = inputArray.map(Number);
+    let sumNoTax = 0;
+    let sumTax = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] < 0) {
+            console.log("Invalid price!");
+            continue;
+        }
+        sumNoTax += inputArray[i];
+    }
+    if (sumNoTax === 0) {
+        console.log("Invalid order!")
+    } else {
+        sumTax = 1.2 * sumNoTax;
+        if (customerType === "special") {
+            sumTax *= 0.9;
+        }
+        console.log("Congratulations you've just bought a new computer!");
+        console.log(`Price without taxes: ${sumNoTax.toFixed(2)}$`);
+        console.log(`Taxes: ${(1.2 * sumNoTax - sumNoTax).toFixed(2)}$`);
+        console.log("-----------");
+        console.log(`Total price: ${sumTax.toFixed(2)}$`);
+    }
 }
 solve([
     '1050',
