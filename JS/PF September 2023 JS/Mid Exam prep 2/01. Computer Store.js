714a07609e361b237fae24abc65052bfae3da6f9
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-20 18:04:08 +0300

    Task 1 prep

---
+++ JS/PF September 2023 JS/Mid Exam prep 2/01. Computer Store.js
@@ -0,0 +1,18 @@
+function solve(inputArray) {
+let customerType = inputArray.pop();
+console.log(customerType);
+inputArray.map(Number);
+if (inputArray.includes(x => x < 0)){
+    
+}
+
+}
+solve([
+    '1050',
+    '200',
+    '450',
+    '2',
+    '18.50',
+    '16.86',
+    'special'
+])
