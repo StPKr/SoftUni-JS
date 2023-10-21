8836c69eb2002dcbfaf93c437f4bfa17b1ab6c67
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-21 16:18:12 +0300

    Task 2 pre

--- JS/PF September 2023 JS/18 Arrays Advanced - More Exercises/02. Bunny Kill.js
+++ JS/PF September 2023 JS/18 Arrays Advanced - More Exercises/02. Bunny Kill.js
@@ -1,13 +1,11 @@
 function bunnyKill(inputArray) {
-let bombBunnycoordinates = inputArray.pop().split(" ");
-inputArray.map(x => Number(x).toArray());
-console.log(inputArray)
-
+    let bombBunnycoordinates = inputArray.pop().split(" ");
+    let convertedArray = inputArray[0].split(" ").map(Number);
 
 }
 bunnyKill(['5 10 15 20',
-           '10 10 10 10',
-           '10 15 10 10',
-           '10 10 10 10',
-           '2,2 0,1']
+    '10 10 10 10',
+    '10 15 10 10',
+    '10 10 10 10',
+    '2,2 0,1']
 );
