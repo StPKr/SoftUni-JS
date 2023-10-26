e14f7323e128905cea4cef913cb748d0c172749b
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-26 17:44:29 +0300

    Objects Task 1

---
+++ JS/PF September 2023 JS/22 Objects and Classes - Lab/01. Person Info.js
@@ -0,0 +1,12 @@
+function personInfo(a, b, x) {
+    let person = {};
+    person.firstName = a;
+    person.lastName = b;
+    person.age = x;
+
+    return person;
+}
+personInfo("Peter",
+    "Pan",
+    "20"
+)
