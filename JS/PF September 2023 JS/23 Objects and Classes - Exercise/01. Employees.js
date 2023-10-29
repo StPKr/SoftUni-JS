5dc40b62cf3564ef7577c14a576851db4e342897
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-29 23:24:43 +0200

    Task 1

---
+++ JS/PF September 2023 JS/23 Objects and Classes - Exercise/01. Employees.js
@@ -0,0 +1,28 @@
+function solve(inputArr) {
+    class Employee {
+        constructor(a, b) {
+            this.name = a;
+            this.personalNumber = b;
+        }
+        info() {
+            console.log(`Name: ${this.name} -- Personal Number: ${this.personalNumber}`);
+        }
+    }
+    let employees = [];
+    for (let el of inputArr) {
+        let name = el;
+        let perNum = el.length;
+        let employee = new Employee(name, perNum);
+        employees.push(employee);
+    }
+    for (let el of employees) {
+        el.info();
+    }
+}
+solve([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+]
+)
