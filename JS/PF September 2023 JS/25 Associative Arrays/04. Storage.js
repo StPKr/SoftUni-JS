7fd19e6a475583ecf7374f2a4bbe84caeebecd03
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-05 22:09:03 +0200

    Task 4

--- JS/PF September 2023 JS/25 Associative Arrays/04. Storage.js
+++ JS/PF September 2023 JS/25 Associative Arrays/04. Storage.js
@@ -1,5 +1,16 @@
 function solve(input) {
-
+    let list = {};
+    for (let item of input) {
+        let [name, amount] = item.split(' ');
+        if (list.hasOwnProperty(name)) {
+            list[name] += Number(amount);
+        } else {
+            list[name] = Number(amount);
+        }
+    }
+    for (let entry of Object.entries(list)) {
+        console.log(entry.join(' -> '));
+    }
 }
 solve(['tomatoes 10',
     'coffee 5',
