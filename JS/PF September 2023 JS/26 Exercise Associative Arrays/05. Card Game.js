e9a99a20f1a3841cfe57572ea672498dfe2402fb
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-06 22:38:46 +0200

    Task 5

--- JS/PF September 2023 JS/26 Exercise Associative Arrays/05. Card Game.js
+++ JS/PF September 2023 JS/26 Exercise Associative Arrays/05. Card Game.js
@@ -12,10 +12,42 @@ function solve(input) {
             list[personName] = cardValues;
         }
     }
-    console.log(list.Peter)
-    let mySet = new Set(list.Peter);
-    console.log(mySet)
-
+    for (let el in list) {
+        let mySet = new Set(list[el]);
+        let sum = 0;
+        for (let item of mySet) {
+            let power = item[0];
+            let type = item[1];
+            let powerValue = 0;
+            let typeValue = 0;
+            if (power === "J") {
+                powerValue = 11;
+            } else if (power === "Q") {
+                powerValue = 12;
+            } else if (power === "K") {
+                powerValue = 13;
+            } else if (power === "A") {
+                powerValue = 14;
+            } else if (power === "1") {
+                powerValue = 10;
+                type = item[2];
+            }
+            else {
+                powerValue = power;
+            }
+            if (type === "S") {
+                typeValue = 4;
+            } else if (type === "H") {
+                typeValue = 3;
+            } else if (type === "D") {
+                typeValue = 2;
+            } else if (type === "C") {
+                typeValue = 1;
+            }
+            sum += powerValue * typeValue;
+        }
+        console.log(`${el}: ${sum}`);
+    }
 }
 solve([
     'Peter: 2C, 4H, 9H, AS, QS',
@@ -26,3 +58,15 @@ solve([
     'Peter: JD, JD, JD, JD, JD, JD'
 ]
 )
+// solve([
+//     'John: 2C, 4H, 9H, AS, QS',
+//     'Slav: 3H, 10S, JC, KD, 5S, 10S',
+//     'Alex: 6H, 7S, KC, KD, 5S, 10C',
+//     'Thomas: QH, QC, JS, JD, JC',
+//     'Slav: 6H, 7S, KC, KD, 5S, 10C',
+//     'Thomas: QH, QC, JS, JD, JC',
+//     'Alex: 6H, 7S, KC, KD, 5S, 10C',
+//     'Thomas: QH, QC, JS, JD, JC',
+//     'John: JD, JD, JD, JD'
+// ]
+// )
