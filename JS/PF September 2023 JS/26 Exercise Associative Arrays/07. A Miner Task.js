506a7c31cd76372c911fefa382596e935787e046
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-07 23:18:41 +0200

    Task 1

--- JS/PF September 2023 JS/26 Exercise Associative Arrays/07. A Miner Task.js
+++ JS/PF September 2023 JS/26 Exercise Associative Arrays/07. A Miner Task.js
@@ -3,6 +3,7 @@ function solve(inputArray) {
     for (let i = 0; i < inputArray.length; i += 2) {
         let resource = inputArray[i];
         let quantity = Number(inputArray[i + 1]);
+        // if (resource in list){ } - same as beloww
         if (list.hasOwnProperty(resource)) {
             list[resource] += quantity;
         } else {
