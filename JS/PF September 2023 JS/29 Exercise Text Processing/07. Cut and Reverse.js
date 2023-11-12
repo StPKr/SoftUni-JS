2cc4069c4147b41c3ae0a6f1d0cec82655eb33dd
Codedelayer <137268124+Codedelayer@users.noreply.github.com>
2023-11-12 15:02:25 +0200

    Task 7

--- JS/PF September 2023 JS/29 Exercise Text Processing/07. Cut and Reverse.js
+++ JS/PF September 2023 JS/29 Exercise Text Processing/07. Cut and Reverse.js
@@ -1,15 +1,21 @@
 function solve(input) {
-    let firstIndex = input.length / 2 - 1;
-    let secondindex = input.length - 1;
-    let firstHalf = "";
-    let secondHalf = "";
-    for (let i = firstIndex; i >= 0; i--) {
-        firstHalf += input[i];
+    // let firstIndex = input.length / 2 - 1;
+    // let secondindex = input.length - 1;
+    // let firstHalf = "";
+    // let secondHalf = "";
+    // for (let i = firstIndex; i >= 0; i--) {
+    //     firstHalf += input[i];
+    // }
+    // for (let j = secondindex; j > firstIndex; j--) {
+    //     secondHalf += input[j];
+    // }
+    // console.log(firstHalf);
+    // console.log(secondHalf);
+    let text = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+        text += input[i];
     }
-    for (let j = secondindex; j > firstIndex; j--) {
-        secondHalf += input[j];
-    }
-    console.log(firstHalf);
-    console.log(secondHalf);
+    console.log(text.slice(input.length / 2));
+    console.log(text.slice(0, input.length / 2));
 }
 solve('tluciffiDsIsihTgnizamAoSsIsihT');
