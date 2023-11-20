c388d90568ead35fe961315f3b1d4a5eca32a7e9
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-20 17:51:38 +0200

    Task 2

--- JS/PF September 2023 JS/32 Exercise Regular Expressions/02. Race.js
+++ JS/PF September 2023 JS/32 Exercise Regular Expressions/02. Race.js
@@ -1,5 +1,28 @@
-function solve() {
+function solve(arr) {
+    let names = arr.shift().split(", ");
+    let command = arr.shift();
+    let results = {};
+    names.forEach(name => results[name] = 0);
+    let letterPattern = /[A-Za-z]/g; // /[a-z]/gi - i means case insensitive
+    let digitPattern = /\d/g;
+    while (command != "end of race") {
+        let letterMatches = command.match(letterPattern);
+        let name = letterMatches.join("");
 
+        let digitMatches = command.match(digitPattern);
+        let distance = digitMatches.map(Number).reduce((acc, val) => acc + val);
+
+        if (name in results) {
+            results[name] += distance;
+        }
+
+        command = arr.shift();
+    }
+    let sortedResults = Object.entries(results).sort((a,b) => b[1] - a[1]);
+
+    console.log(`1st place: ${sortedResults[0][0]}`);
+    console.log(`2nd place: ${sortedResults[1][0]}`);
+    console.log(`3rd place: ${sortedResults[2][0]}`);
 }
 solve(['George, Peter, Bill, Tom',
     'G4e@55or%6g6!68e!!@ ',
