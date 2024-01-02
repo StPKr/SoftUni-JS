507a27e6cb8ad1bea0458334c3b8e72ec9d5da26
Public Name <137268124+Codedelayer@users.noreply.github.com>
2024-01-02 21:36:17 +0200

    Task 2

---
+++ JS/PA January 2024 JS/03 Syntax, Functions and Statements/02. String Length.js
@@ -0,0 +1,6 @@
+function solve(a, b, c) {
+    let average = (a.length + b.length + c.length) / 3;
+    console.log(a.length + b.length + c.length);
+    console.log(Math.floor(average));
+}
+solve('chocolate', 'ice cream', 'cake');
