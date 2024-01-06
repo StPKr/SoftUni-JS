5cb5dde2758da336cc29a0d6133ea57c30ce2012
Public Name <137268124+Codedelayer@users.noreply.github.com>
2024-01-06 22:54:53 +0200

    Task 7

---
+++ JS/PA January 2024 JS/05. Arrays and Nested Arrays/07. Piece of Pie.js
@@ -0,0 +1,22 @@
+function solve(inputArray, a, b) {
+    let firstIndex = inputArray.indexOf(a);
+    let lastIndex = inputArray.indexOf(b);
+    return inputArray.slice(firstIndex, lastIndex + 1);
+};
+solve(['Pumpkin Pie',
+    'Key Lime Pie',
+    'Cherry Pie',
+    'Lemon Meringue Pie',
+    'Sugar Cream Pie'],
+    'Key Lime Pie',
+    'Lemon Meringue Pie'
+);
+solve(['Apple Crisp',
+    'Mississippi Mud Pie',
+    'Pot Pie',
+    'Steak and Cheese Pie',
+    'Butter Chicken Pie',
+    'Smoked Fish Pie'],
+    'Pot Pie',
+    'Smoked Fish Pie'
+);
