f09a4b211458655f22bae0fff81dcb34dd32d03f
Public Name <137268124+Codedelayer@users.noreply.github.com>
2024-01-07 18:55:15 +0200

    Create 03. City Taxes.js

---
+++ JS/PA January 2024 JS/07. Objects and Composition - Lab/03. City Taxes.js
@@ -0,0 +1,24 @@
+function solve(name, population, treasury) {
+    return {
+        name, population, treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percent) {
+            this.population += Math.floor(this.population * percent / 100);
+        },
+        applyRecession(percent) {
+            this.treasury -= Math.floor(this.treasury * percent / 100);
+        },
+    };
+};
+const city =
+    solve('Tortuga',
+        7000,
+        15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
+
