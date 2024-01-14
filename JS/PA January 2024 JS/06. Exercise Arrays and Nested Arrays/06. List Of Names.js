49142531b09874facf8de98cb2c1f6090c27b687
Public Name <137268124+Codedelayer@users.noreply.github.com>
2024-01-14 16:45:37 +0200

    Update 06. List Of Names.js

--- JS/PA January 2024 JS/06. Exercise Arrays and Nested Arrays/06. List Of Names.js
+++ JS/PA January 2024 JS/06. Exercise Arrays and Nested Arrays/06. List Of Names.js
@@ -1,4 +1,7 @@
-function solve() {
-    
+function solve(inputArray) {
+    inputArray.sort((a, b) => a.localeCompare(b));
+    for (let i = 1; i <= inputArray.length; i++) {
+        console.log(i + "." + inputArray[i - 1]);
+    }
 }
 solve(["John", "Bob", "Christina", "Ema"]);
