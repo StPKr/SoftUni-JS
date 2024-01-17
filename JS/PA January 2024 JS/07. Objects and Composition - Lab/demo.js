2beb1dbd94efcfe6fa4a0bce4e7c90d2737f9ca8
Public Name <137268124+Codedelayer@users.noreply.github.com>
2024-01-17 20:47:48 +0200

    Update demo.js

--- JS/PA January 2024 JS/07. Objects and Composition - Lab/demo.js
+++ JS/PA January 2024 JS/07. Objects and Composition - Lab/demo.js
@@ -1,27 +1,35 @@
-function bye(){
-    console.log("Bye!");
+// function bye(){
+//     console.log("Bye!");
+// }
+// const person = {
+//     name: "Peter",
+//     age: 24,
+//     'last name': 'Johnson',
+//     sayHi() {
+//         console.log(`Hello! ${this.name}`);
+//     },
+//     sayBye: bye
+// };
+
+// person.sayHi();
+
+// console.log(person['last name']);
+// const myFn = person.sayHi;
+// myFn(); 
+
+// person.sayBye();
+
+// const person2 = {
+//     name: 'Jon',
+//     age: 31,
+//     sayBye: bye
+// }
+
+function createRect(width, height) {
+    const rect = { width, height };
+    rect.getArea = () => {
+        return rect.width * rect.height;
+    };
+    return rect;
 }
-const person = {
-    name: "Peter",
-    age: 24,
-    'last name': 'Johnson',
-    sayHi() {
-        console.log(`Hello! ${this.name}`);
-    },
-    sayBye: bye
-};
-
-person.sayHi();
-
-console.log(person['last name']);
-const myFn = person.sayHi;
-myFn(); 
-
-person.sayBye();
-
-const person2 = {
-    name: 'Jon',
-    age: 31,
-    sayBye: bye
-}
-
+createRect(4, 7);
