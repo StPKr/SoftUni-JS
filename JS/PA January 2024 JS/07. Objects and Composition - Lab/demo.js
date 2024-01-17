42f0f43987d926eb34e869116716a64e7326e9e2
Public Name <137268124+Codedelayer@users.noreply.github.com>
2024-01-17 21:07:58 +0200

    5

--- JS/PA January 2024 JS/07. Objects and Composition - Lab/demo.js
+++ JS/PA January 2024 JS/07. Objects and Composition - Lab/demo.js
@@ -25,11 +25,28 @@
 //     sayBye: bye
 // }
 
-function createRect(width, height) {
-    const rect = { width, height };
-    rect.getArea = () => {
-        return rect.width * rect.height;
-    };
-    return rect;
-}
-createRect(4, 7);
+// function createRect(width, height) {
+//     const rect = { width, height };
+//     rect.getArea = () => {
+//         return rect.width * rect.height;
+//     };
+//     return rect;
+// }
+// createRect(4, 7);
+// function myDecorator(func) {
+//     return function() {
+//       console.log("Decorator added some functionality");
+//       func(); // Call the original function
+//     };
+//   }
+  
+//   // Function to be decorated
+//   function myFunction() {
+//     console.log("Original function");
+//   }
+  
+//   // Decorate the function
+//   const decoratedFunction = myDecorator(myFunction);
+  
+//   // Call the decorated function
+//   decoratedFunction();
