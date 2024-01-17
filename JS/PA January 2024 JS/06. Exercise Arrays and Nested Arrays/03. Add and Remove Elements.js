7f3ffc4f35c433b3cd3a9fde56ee5a0a38f132a9
Public Name <137268124+Codedelayer@users.noreply.github.com>
2024-01-17 19:38:48 +0200

    Update 03. Add and Remove Elements.js

--- JS/PA January 2024 JS/06. Exercise Arrays and Nested Arrays/03. Add and Remove Elements.js
+++ JS/PA January 2024 JS/06. Exercise Arrays and Nested Arrays/03. Add and Remove Elements.js
@@ -2,12 +2,17 @@ function solve(inputArray) {
     let outputArray = [];
     for (let i = 1; i <= inputArray.length; i++) {
         let command = inputArray[i - 1];
-        switch (command) {
-            case "add": outputArray.push(i); break;
-            case "remove": outputArray.pop(); break;
+        // switch (command) {
+        //     case "add": outputArray.push(i); break;
+        //     case "remove": outputArray.pop(); break;
+        // }
+        const tester = {
+            add() { outputArray.push(i) },
+            remove() { outputArray.pop() }
         }
+        tester[command]();
     }
-    if (outputArray.length > 0){
+    if (outputArray.length > 0) {
         console.log(outputArray.join("\n"));
     } else {
         console.log("Empty");
