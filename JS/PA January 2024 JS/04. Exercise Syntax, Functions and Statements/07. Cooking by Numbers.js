676d57e0e42e37c1b56914ce4beed029f483aafd
Public Name <137268124+Codedelayer@users.noreply.github.com>
2024-01-17 19:41:17 +0200

    Update 07. Cooking by Numbers.js

--- JS/PA January 2024 JS/04. Exercise Syntax, Functions and Statements/07. Cooking by Numbers.js
+++ JS/PA January 2024 JS/04. Exercise Syntax, Functions and Statements/07. Cooking by Numbers.js
@@ -2,13 +2,21 @@ function solve(...input) {
     let n = Number(input.shift());
     while (input.length > 0) {
         let command = input.shift();
-        switch (command) {
-            case "chop": n /= 2; console.log(n); break;
-            case "dice": n = Math.sqrt(n); console.log(n); break;
-            case "spice": n += 1; console.log(n); break;
-            case "bake": n *= 3; console.log(n); break;
-            case "fillet": n *= 0.8; console.log(n); break;
+        // switch (command) {
+        //     case "chop": n /= 2; console.log(n); break;
+        //     case "dice": n = Math.sqrt(n); console.log(n); break;
+        //     case "spice": n += 1; console.log(n); break;
+        //     case "bake": n *= 3; console.log(n); break;
+        //     case "fillet": n *= 0.8; console.log(n); break;
+        // }
+        const tester = {
+            chop() { n /= 2; console.log(n) },
+            dice() { n = Math.sqrt(n); console.log(n) },
+            spice() { n += 1; console.log(n) },
+            bake() { n *= 3; console.log(n) },
+            fillet() { n *= 0.8; console.log(n) }
         }
+        tester[command]();
     }
 }
 solve('32', 'chop', 'chop', 'chop', 'chop', 'chop')
