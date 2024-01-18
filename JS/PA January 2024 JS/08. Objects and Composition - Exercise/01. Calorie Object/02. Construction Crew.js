a4e8996804ed8eaf9d803d12594940b0cc12cac9
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-18 07:30:34 +0200

    Create 02. Construction Crew.js

---
+++ JS/PA January 2024 JS/08. Objects and Composition - Exercise/01. Calorie Object/02. Construction Crew.js
@@ -0,0 +1,14 @@
+function solve(object) {
+    if (object.dizziness) {
+        object.levelOfHydrated += 0.1 * object.experience * object.weight;
+        object.dizziness = false;
+    }
+    return object;
+}
+solve({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+}
+)
