47b90f7234acf0efc618aa6456f1ade7e2eb49e4
Codedelayer <137268124+Codedelayer@users.noreply.github.com>
2024-01-19 18:44:38 +0200

    Create 09. Sorted List.js

---
+++ JS/PA January 2024 JS/08. Objects and Composition - Exercise/01. Calorie Object/09. Sorted List.js
@@ -0,0 +1,31 @@
+function createSortedList() {
+    let result = {
+        list: [],
+        size: 0,
+        add(n) {
+            result.list.push(n);
+            result.list.sort((a, b) => a - b);
+            result.size = result.list.length;
+        },
+        remove(n) {
+            if (n >= 0 && n < result.list.length) {
+                result.list.splice(n, 1);
+                result.size = result.list.length;
+            }
+        },
+        get(n) {
+            if (result.list.length >= n + 1) {
+                return this.list[n]
+            }
+        },
+    }
+    return result;
+}
+let list = createSortedList();
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(1));
+list.remove(1);
+console.log(list.get(1));
+
