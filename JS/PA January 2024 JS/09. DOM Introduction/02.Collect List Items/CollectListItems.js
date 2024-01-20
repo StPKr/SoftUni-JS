cf17b54222cc479fdd76d3635201e539ede0ebf7
Codedelayer <137268124+StPKr@users.noreply.github.com>
2024-01-20 13:57:52 +0200

    Update CollectListItems.js

--- JS/PA January 2024 JS/09. DOM Introduction/02.Collect List Items/CollectListItems.js
+++ JS/PA January 2024 JS/09. DOM Introduction/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,10 @@
 function extractText() {
-    // TODO
+    let result = [];
+    const list = document.getElementById('items');
+    const items = Array.from(list.children);
+    for (let item of items) {
+        result.push(item.textContent);
+    }
+    const textarea = document.getElementById('result');
+    textarea.value = result.join('\n');
 }
