428b6a7c26e8604aa800a78fe640ace54fe700cb
Codedelayer <137268124+StPKr@users.noreply.github.com>
2024-01-20 12:44:38 +0200

    Task 1

--- JS/PA January 2024 JS/09. DOM Introduction/01.Edit Element/EditElement.js
+++ JS/PA January 2024 JS/09. DOM Introduction/01.Edit Element/EditElement.js
@@ -1,3 +1,4 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+    let content = element.textContent;
+    element.textContent = content.split(match).join(replacer);
 }
