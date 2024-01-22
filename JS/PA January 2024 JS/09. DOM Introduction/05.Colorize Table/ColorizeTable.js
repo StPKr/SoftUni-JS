0fb6dbccbf3b89669a302d959c65cc46e8e8eda9
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-22 21:58:05 +0200

    Update ColorizeTable.js

--- JS/PA January 2024 JS/09. DOM Introduction/05.Colorize Table/ColorizeTable.js
+++ JS/PA January 2024 JS/09. DOM Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,6 +1,11 @@
 function colorize() {
-    const rows = Array.from(document.getElementsByTagName('tr'));
-    for (let i = 1; i < rows.length; i += 2) {
-        rows[i].style.background = "teal";
+    // const rows = Array.from(document.getElementsByTagName('tr'));
+    // for (let i = 1; i < rows.length; i += 2) {
+    //     rows[i].style.background = "teal";
+    // }
+    const rows = document.querySelectorAll('tr:nth-child(even)');
+    for (let row of rows) {
+        row.style.background = 'teal';
     }
+
 }
