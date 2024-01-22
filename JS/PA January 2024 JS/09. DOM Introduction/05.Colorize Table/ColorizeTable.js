bb131be7ed323448c9ce192596700e4409ebaa0c
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-22 21:50:57 +0200

    Update ColorizeTable.js

--- JS/PA January 2024 JS/09. DOM Introduction/05.Colorize Table/ColorizeTable.js
+++ JS/PA January 2024 JS/09. DOM Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,6 @@
 function colorize() {
-    // TODO
+    const rows = Array.from(document.getElementsByTagName('tr'));
+    for (let i = 1; i < rows.length; i += 2) {
+        rows[i].style.background = "teal";
+    }
 }
