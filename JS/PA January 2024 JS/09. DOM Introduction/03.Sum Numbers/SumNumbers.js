055e61ab76f1a90976166811838a6382daee0a8d
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-22 21:28:50 +0200

    Update SumNumbers.js

--- JS/PA January 2024 JS/09. DOM Introduction/03.Sum Numbers/SumNumbers.js
+++ JS/PA January 2024 JS/09. DOM Introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,6 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const result = Number(num1) + Number(num2);
+    document.getElementById("sum").value = result;
 }
