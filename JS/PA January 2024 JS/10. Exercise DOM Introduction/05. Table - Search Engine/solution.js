a6b9e227fc21b5727070b94d8d35a9b1c7f1b3f6
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-22 23:42:58 +0200

    Update solution.js

--- JS/PA January 2024 JS/10. Exercise DOM Introduction/05. Table - Search Engine/solution.js
+++ JS/PA January 2024 JS/10. Exercise DOM Introduction/05. Table - Search Engine/solution.js
@@ -1,8 +1,21 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
+   const searchInputRef = document.getElementById("searchField");
+   const tableRows = Array.from(document.querySelectorAll("tbody tr"));
    function onClick() {
-      //   TODO:
+      let searchText = searchInputRef.value;
+      for (let tableRow of tableRows){
+         let tableData = Array.from(tableRow.querySelectorAll("td"));
+         for (let data of tableData){
+            if (data.textContent.includes(searchText)){
+               tableRow.classList.add("select");
+               break;
+            } else {
+               tableRow.classList.remove("select");
+            }
 
+         }
+      }
+      searchInputRef.value = "";
    }
 }
