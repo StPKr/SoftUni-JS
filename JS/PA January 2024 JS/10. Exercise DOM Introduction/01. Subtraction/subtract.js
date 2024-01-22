cfa37b25ed07633a071cd6946c3ba896d9c72d52
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-22 22:50:05 +0200

    Update

--- JS/PA January 2024 JS/10. Exercise DOM Introduction/01. Subtraction/subtract.js
+++ JS/PA January 2024 JS/10. Exercise DOM Introduction/01. Subtraction/subtract.js
@@ -1,3 +1,7 @@
 function subtract() {
-    console.log('TODO:...');
+    debugger
+    const num1 = Number(document.getElementById('firstNumber').value);
+    const num2 = Number(document.getElementById('secondNumber').value);
+    const sum = num1 - num2;
+    document.getElementById("result").textContent = sum;
 }
