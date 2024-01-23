7f722dbaec8ef5721d05c87295e85879d27da148
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-23 22:43:42 +0200

    Update app.js

--- JS/PA January 2024 JS/11. DOM Manipulations and Events/01. List-Of-Items/app.js
+++ JS/PA January 2024 JS/11. DOM Manipulations and Events/01. List-Of-Items/app.js
@@ -4,8 +4,8 @@ function addItem() {
 
     if (input.value.length == 0) {
         return;
-    }
-    
+    } // - stops the button from adding an empty field if there is no input
+
     const liElement = document.createElement('li');
     liElement.textContent = text;
     const list = document.getElementById('items');
