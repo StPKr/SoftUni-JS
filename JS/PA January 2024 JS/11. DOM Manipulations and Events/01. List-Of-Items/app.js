8557cc60fd5801624598cbfcf59f3475cd88e0e7
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-23 22:42:56 +0200

    Update app.js

--- JS/PA January 2024 JS/11. DOM Manipulations and Events/01. List-Of-Items/app.js
+++ JS/PA January 2024 JS/11. DOM Manipulations and Events/01. List-Of-Items/app.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    const input = document.getElementById('newItemText');
+    const text = input.value;
+
+    if (input.value.length == 0) {
+        return;
+    }
+    
+    const liElement = document.createElement('li');
+    liElement.textContent = text;
+    const list = document.getElementById('items');
+    list.appendChild(liElement);
+    input.value = "";
 }
