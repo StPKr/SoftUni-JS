c624a90bc0a935d088d0014632b1b0104070e33d
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-23 23:06:16 +0200

    Update app.js

--- JS/PA January 2024 JS/11. DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ JS/PA January 2024 JS/11. DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -2,18 +2,13 @@ function deleteByEmail() {
     const input = document.querySelector('[name="email"]');
     const table = document.getElementById('customers');
     const rows = Array.from(table.querySelectorAll('tbody tr'));
-    let success = false;
+    let result = "Not found.";
     for (let row of rows) {
-        if (row.children[1].textContent == input.value){
+        if (row.children[1].textContent == input.value) {
             row.remove();
-            success = true;
+            result = "Deleted";
         }
     }
     const output = document.getElementById('result');
-    if (success){
-        output.textContent = 'Deleted.';
-    } else {
-
-        output.textContent = 'Not found.';
-    }
+    output.textContent = result;
 }
