548e6e292a9c325bc47cf807598aa171667ea04a
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-24 22:03:13 +0200

    Update

--- JS/PA January 2024 JS/11. DOM Manipulations and Events/03. Add-Delete/app.js
+++ JS/PA January 2024 JS/11. DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,29 @@
 function addItem() {
-    //TODO...
+    const input = document.getElementById('newItemText');
+    const text = input.value;
+
+    if (input.value.length == 0) {
+        return;
+    } // - stops the button from adding an empty field if there is no input
+
+    const liElement = document.createElement('li');
+    liElement.textContent = text;
+
+    //begin 03
+    const deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#';
+    deleteBtn.addEventListener('click', onDelete);
+    liElement.appendChild(deleteBtn);
+    //end 03.
+
+    const list = document.getElementById('items');
+    list.appendChild(liElement);
+    input.value = "";
+
+    function onDelete(event) {
+        const deleteBtn = event.target;
+        const liElement = deleteBtn.parentElement;
+        liElement.remove();
+    }
 }
