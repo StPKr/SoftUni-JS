bb762cdb63fe747e9e09f8ca588ae1973ccae1b5
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-25 22:01:38 +0200

    Update script.js

--- JS/PA January 2024 JS/12. Exercise DOM Manipulations and Events/01. Sections/script.js
+++ JS/PA January 2024 JS/12. Exercise DOM Manipulations and Events/01. Sections/script.js
@@ -1,3 +1,19 @@
 function create(words) {
-   console.log('TODO:...');
+   for (let el of words) {
+      const div = document.createElement("div");
+      const paragraph = document.createElement('p');
+      paragraph.style.display = 'none';
+      paragraph.textContent = el;
+      div.appendChild(paragraph);
+      const list = document.getElementById('content');
+      list.appendChild(div);
+   }
+   let divList = Array.from(document.getElementById('content').children);
+   for (let el of divList) {
+      el.addEventListener('click', showText);
+   }
+   function showText(e){
+      const div = e.target;
+      div.children[0].style.display = "block";
+   }
 }
