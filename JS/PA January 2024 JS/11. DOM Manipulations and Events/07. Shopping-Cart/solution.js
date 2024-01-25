a5c5fdabadbe40e0a7edfe6af612bba4addb5cf1
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-25 21:00:41 +0200

    Update solution.js

--- JS/PA January 2024 JS/11. DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ JS/PA January 2024 JS/11. DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,38 @@
 function solve() {
-   //TODO...
+	let shoppingCart = document.getElementsByClassName("shopping-cart")[0];
+	let result = document.getElementsByTagName("textarea")[0];
+	let products = [];
+	let totalPrice = 0;
+	let checkoutDone = false;
+
+	shoppingCart.addEventListener("click", function (event) {
+		if (event.target.nodeName !== "BUTTON" || checkoutDone) {
+			return;
+		}
+		
+		let btn = event.target;
+		if (Array.from(btn.classList).includes("add-product")) {
+			let productElement = event.target.parentElement.parentElement;
+			let name =
+				productElement.querySelectorAll(".product-title")[0]
+					.textContent;
+			let price = productElement.querySelectorAll(
+				".product-line-price"
+			)[0].textContent;
+
+			result.textContent += `Added ${name} for ${price} to the cart.\n`;
+
+			if (!products.includes(name)) {
+				products.push(name);
+			}
+
+			totalPrice += Number(price);
+		} else if (Array.from(btn.classList).includes("checkout")) {
+			let list = products.join(", ");
+			result.textContent += `You bought ${list} for ${totalPrice.toFixed(
+				2
+			)}.`;
+			checkoutDone = true;
+		}
+	});
 }
