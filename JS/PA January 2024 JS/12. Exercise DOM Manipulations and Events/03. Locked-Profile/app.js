63357c0820df7d81f2433201d357decb20e32fd7
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-26 12:35:06 +0200

    Update app.js

--- JS/PA January 2024 JS/12. Exercise DOM Manipulations and Events/03. Locked-Profile/app.js
+++ JS/PA January 2024 JS/12. Exercise DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,19 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const btns = Array.from(document.querySelectorAll("button"));
+    btns.forEach(x => x.addEventListener('click', onClickHandler))
+
+    function onClickHandler(e) {
+        let hiddenInfo = e.target.parentElement.querySelector("div");
+        let currentRadioBtn = e.target.parentElement.querySelector("input[type='radio']:checked");
+        
+        if(currentRadioBtn.value === "unlock"){
+            if (e.currentTarget.textContent === "Show more"){
+                e.currentTarget.textContent = "Hide it";
+                hiddenInfo.style.display = "block";
+            } else {
+                e.currentTarget.textContent = "Show more";
+                hiddenInfo.style.display = "none";
+            }
+        }
+    }
 }
