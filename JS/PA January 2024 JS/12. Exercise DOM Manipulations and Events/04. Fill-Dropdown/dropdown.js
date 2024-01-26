010a7b8de9bdea62b7613285e82e6c524af6bb9d
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-26 13:41:57 +0200

    Update dropdown.js

--- JS/PA January 2024 JS/12. Exercise DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
+++ JS/PA January 2024 JS/12. Exercise DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const selectMenuRef = document.getElementById("menu");
+    const textInputRef = document.getElementById("newItemText");
+    const keyInputRef = document.getElementById("newItemValue");
+    let textValue = textInputRef.value;
+    let keyValue = keyInputRef.value;
+    let option = document.createElement('option');
+    option.textContent = textValue;
+    option.value = keyValue;
+    selectMenuRef.appendChild(option);
+    textInputRef.value = "";
+    keyInputRef.value = "";
 }
