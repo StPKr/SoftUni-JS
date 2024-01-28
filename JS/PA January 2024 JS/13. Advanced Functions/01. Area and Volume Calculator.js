74ffbdf36bba5f0e9d2254bb0146df6b690918b7
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-28 15:04:52 +0200

    Update 01. Area and Volume Calculator.js

--- JS/PA January 2024 JS/13. Advanced Functions/01. Area and Volume Calculator.js
+++ JS/PA January 2024 JS/13. Advanced Functions/01. Area and Volume Calculator.js
@@ -1,13 +1,19 @@
 function solve(areaFn, volFn, input) {
     const data = JSON.parse(input);
-    const result = [];
+    // const result = [];
 
-    for (let fig of data) {
-        result.push({
-            area: areaFn.call(fig),
-            volume: volFn.call(fig)
-        })
-    }
+    // for (let fig of data) {
+    //     result.push({
+    //         area: areaFn.call(fig),
+    //         volume: volFn.call(fig)
+    //     })
+    // } - noob way
+
+    const result = data.map(fig => ({
+        area: areaFn.call(fig),
+        volume: volFn.call(fig)
+    }));
+
     return result
 }
 
