27c41f3259835e849d2dca3f3f1045ea59efbf4e
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-29 22:10:14 +0200

    Update 01. Sort Array.js

--- JS/PA January 2024 JS/14. Exercise Advanced Functions/01. Sort Array.js
+++ JS/PA January 2024 JS/14. Exercise Advanced Functions/01. Sort Array.js
@@ -12,3 +12,9 @@ function sorter(inputArray, arg) {
     return inputArray;
 }
 sorter([14, 7, 17, 6, 8], 'asc');
+
+// function sortArray(arr, type) {
+// 	return type === "asc"
+// 		? arr.sort((a, b) => a - b)
+// 		: arr.sort((a, b) => b - a);
+// } - even shorter
