988a734c386c667120671b98b389fd85e2f2a16e
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-30 23:22:22 +0200

    Update script.js

--- JS/PA January 2024 JS/14. Exercise Advanced Functions/08. Next Article/script.js
+++ JS/PA January 2024 JS/14. Exercise Advanced Functions/08. Next Article/script.js
@@ -1,3 +1,15 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let articlesContent = Array.from(articles);
+    const contentRef = document.getElementById('content');
+
+    return () => {
+        if (!articlesContent.length) { //0
+            return;
+        }
+
+        let currentArticleValue = articlesContent.shift();
+        let newArticle = document.createElement("article");
+        newArticle.textContent = currentArticleValue;
+        contentRef.appendChild(newArticle);
+    }
 }
