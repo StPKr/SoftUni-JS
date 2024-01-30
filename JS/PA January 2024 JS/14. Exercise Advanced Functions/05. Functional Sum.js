1b3b58adfc6f735c2d82877b9f190eb6651bd185
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-30 22:39:08 +0200

    Update 05. Functional Sum.js

--- JS/PA January 2024 JS/14. Exercise Advanced Functions/05. Functional Sum.js
+++ JS/PA January 2024 JS/14. Exercise Advanced Functions/05. Functional Sum.js
@@ -1,15 +1,15 @@
 function add(num) {
-	let sum = num;
+    let sum = num;
 
-	function calc(num2) {
-		sum += num2;
-		return calc;
-	}
-	
-	calc.toString = function () {
-		return sum;
-	};
-	return calc;
+    function calc(num2) {
+        sum += num2;
+        return calc;
+    }
+
+    calc.toString = function () {
+        return sum;
+    };
+    return calc;
 }
-add(1)
-// add(1)(6)(-3)
+// add(1)
+add(1)(6)(-3)
