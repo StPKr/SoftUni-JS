43299d63e61a3d816c5cfa76df88a163fe7ec000
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-30 20:38:31 +0200

    Update test.js

--- JS/PA January 2024 JS/14. Exercise Advanced Functions/test.js
+++ JS/PA January 2024 JS/14. Exercise Advanced Functions/test.js
@@ -1,12 +1,24 @@
-function solve(arr){
-    return function(){
-        return "hello";
-    }
-}
-let myFunc = solve;
-console.log(myFunc);
-console.log(myFunc());
-console.log(); 
-myFunc = solve();
-console.log(myFunc);
-console.log(myFunc());
+// function solve(arr){
+//     return function(){
+//         return "hello";
+//     }
+// }
+// let myFunc = solve;
+// console.log(myFunc);
+// console.log(myFunc());
+// console.log(); 
+// myFunc = solve();
+// console.log(myFunc);
+// console.log(myFunc());
+
+let a = {name: "bill", gender: "male"};
+let b = Object.assign({}, a);
+let c = Object.create(a); // - only the prototype is inherited
+a.name = "Test"
+console.log(a);
+
+console.log(b);
+console.log(b.name);
+
+console.log(c);
+console.log(c.name);
