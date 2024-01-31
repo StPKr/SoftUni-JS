b849c1f8a70ad94137f97698797dd402f2ef7196
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-31 21:46:15 +0200

    Update app.js

--- JS/PA January 2024 JS/14. Exercise Advanced Functions/09. Task Manager/app.js
+++ JS/PA January 2024 JS/14. Exercise Advanced Functions/09. Task Manager/app.js
@@ -1,16 +1,44 @@
 function solve() {
-    const taskInpuRef = document.getElementById('task');
-    const descrInputRef = document.getElementById('description');
-    const dateInputRef = document.getElementById('date');
-    const addBtn = document.getElementById('add');
-    const openDivRef = document.querySelector('.orange');
+    const formRef = document.querySelector('form');
+    const [addTask, openTask, inProgress, completeTask] = document.querySelectorAll('section');
 
-    addBtn.addEventListener('click', addTask);
+    formRef.addEventListener('click', onSubmithandler);
 
-    function addTask() {
-        const h3 = document.createElement('h3');
-        let divSection = openDivRef.parentElement.nextElementSibling;
-        h3.innerHTML = "Test";
-        divSection.appendChild(h3);
+    function onSubmithandler(e) {
+        e.preventDefault();
+        let formElements = formRef.elements;
+        let taskName = formElements[0].value;
+        let desc = formElements[1].value;
+        let date = formElements[2].value;
+
+        if (!taskName || !desc || !date) {
+            return;
+        }
+        createArticle(taskName, desc, date);
+    }
+    function createArticle(name, desc, date) {
+        let newArticle = document.createElement("article");
+        newArticle.innerHTML = getArticleTemp(name, desc, date);
+        openTask.children[1].appendChild(newArticle);
+        addEventListenerToButton();
+    }
+
+    function addEventListenerToButton(btns) {
+        Array.from(btns).forEach(btn => btn.addEventListener('click', clickhandler));
+    }
+
+    function getArticleTemp(name, desc, date) {
+        let btnsPartial = getBtnPartial();
+        return `<h3>${name}</h3>` +
+            `<p>Description: ${desc}</p>` +
+            `<p>Due Date: ${date}</p>` +
+            getBtnPartial()
+    }
+
+    function getBtnPartial() {
+        return `<div class="flex">` +
+            `<button class="green">Start</button>` +
+            `<button class="red">Delete</button>` +
+            `</div>`
     }
 }
