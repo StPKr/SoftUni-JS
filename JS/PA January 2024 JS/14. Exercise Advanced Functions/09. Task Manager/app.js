5910d562644a20a1c8f107cbf60396e8f5d1e7d6
Public Name <137268124+StPKr@users.noreply.github.com>
2024-01-31 16:34:42 +0200

    Таск 7

--- JS/PA January 2024 JS/14. Exercise Advanced Functions/09. Task Manager/app.js
+++ JS/PA January 2024 JS/14. Exercise Advanced Functions/09. Task Manager/app.js
@@ -1,3 +1,16 @@
 function solve() {
-    console.log("//TODO")
+    const taskInpuRef = document.getElementById('task');
+    const descrInputRef = document.getElementById('description');
+    const dateInputRef = document.getElementById('date');
+    const addBtn = document.getElementById('add');
+    const openDivRef = document.querySelector('.orange');
+
+    addBtn.addEventListener('click', addTask);
+
+    function addTask() {
+        const h3 = document.createElement('h3');
+        let divSection = openDivRef.parentElement.nextElementSibling;
+        h3.innerHTML = "Test";
+        divSection.appendChild(h3);
+    }
 }
