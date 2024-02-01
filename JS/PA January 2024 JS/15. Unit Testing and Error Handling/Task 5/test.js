7146fb36f520760d35ad9635da958867f57595c6
Public Name <137268124+StPKr@users.noreply.github.com>
2024-02-01 22:27:58 +0200

    Update

--- JS/PA January 2024 JS/15. Unit Testing and Error Handling/Task 5/test.js
+++ JS/PA January 2024 JS/15. Unit Testing and Error Handling/Task 5/test.js
@@ -4,18 +4,41 @@ import { isSymmetric } from './solution.js';
 describe('Suite', function () {
     it('works with number array', () => {
         const arr = [1, 1, 1];
-        expect(sum(arr)).to.equal(3);
+        expect(isSymmetric(arr)).to.be.true;
     });
-
-    it('throws an error for non-array params', () => {
-        expect(() => sum(string)).to.throw;
+});
+describe('Suite2', function () {
+    it('works with number array2', () => {
+        const arr = [1];
+        expect(isSymmetric(arr)).to.be.true;
     });
-
-    it('return 0 for empty array', () => {
-        expect(sum([])).to.equal(0);
+});
+describe('Suite3', function () {
+    it('works with number array3', () => {
+        const arr = [1, 1, 2];
+        expect(isSymmetric(arr)).to.be.false;
     });
+});
+describe('Suite4', function () {
+    it('works with number array4', () => {
+        const arr = "test";
+        expect(isSymmetric(arr)).to.be.false;
+    });
+});
+describe('Suite5', function () {
+    it('works with number array5', () => {
+        const arr = [];
+        expect(isSymmetric(arr)).to.be.true;
+    });
+});
+describe("General tests", function () {
+    it("should be a function", function () {
+        expect(typeof isSymmetric).to.equal('function');
+    })
+});
 
-    it('return 1 for array with 1 element', () => {
-        expect(sum([1])).to.equal(1);
+describe('Suite33', function () {
+    it("should return true for [5,'hi',{a:5},new Date(),{a:5},'hi',5]", function () {
+        expect(isSymmetric([5, 'hi', { a: 5 }, new Date(), { a: 5 }, 'hi', 5])).to.be.true;
     });
 });
