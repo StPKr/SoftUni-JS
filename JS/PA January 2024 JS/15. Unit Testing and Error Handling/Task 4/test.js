591ba0dc72020d442bb6325f7a2034b60a9f7b00
Public Name <137268124+StPKr@users.noreply.github.com>
2024-02-01 21:59:35 +0200

    Update test.js

--- JS/PA January 2024 JS/15. Unit Testing and Error Handling/Task 4/test.js
+++ JS/PA January 2024 JS/15. Unit Testing and Error Handling/Task 4/test.js
@@ -4,16 +4,17 @@ import { sum } from './solution.js';
 describe('Suite', function () {
     it('works with number array', () => {
         const arr = [1, 1, 1];
+        const string = "test";
 
         expect(sum(arr)).to.equal(3);
     });
 
     it('throws an error for non-array params', () => {
-        expect(() => sum(1).to.throw);
+        expect(() => sum(string).to.throw);
     });
 
     it('return 0 for empty array', () => {
-        expect(sum([])).to.equal(0);
+        expect(sum()).to.equal(0);
     });
 
     it('return 1 for array with 1 element', () => {
