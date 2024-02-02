e9e4d6556e3dd27a4eb7f288108c727c1b5bcce6
Public Name <137268124+StPKr@users.noreply.github.com>
2024-02-02 22:42:11 +0200

    Update app.js

--- JS/PA January 2024 JS/16. Exercise Unit Testing and Error Handling/06.Dynamic-Validation/app.js
+++ JS/PA January 2024 JS/16. Exercise Unit Testing and Error Handling/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    const inputRef = document.getElementById('email');
+    inputRef.addEventListener('change', onChange);
+
+    function onChange(e){
+        let email = e.target.value;
+        let pattern = /[a-z]+@+[a-z]+\.[a-z]+/g;
+        if (pattern.test(email)){
+            e.target.classList.remove('error')
+        } else {
+            e.target.classList.add('error');
+        }
+    }
 }
