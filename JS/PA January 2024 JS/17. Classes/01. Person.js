0c794685781943da57b9501296f7a39bccee22de
Public Name <137268124+StPKr@users.noreply.github.com>
2024-02-03 17:10:32 +0200

    updt

--- JS/PA January 2024 JS/17. Classes/01. Person.js
+++ JS/PA January 2024 JS/17. Classes/01. Person.js
@@ -1,10 +1,14 @@
 class Person {
+    static counter = 0;
+
     firstName;
     lastName;
     age;
     email;
 
     constructor(firstName, lastName, age, email) {
+        Person.counter++;
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
@@ -14,7 +18,39 @@ class Person {
     toString() {
         return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
     }
-}
 
+    sayHello() {
+        console.log(`${this.firstName} says hi!`);
+    }
+    static greet() {
+        console.log('Hello world!');
+        this.farewell(); // can access other static methods but not other ones like sayHello();
+    }
+
+    static farewell() {
+        console.log('Goodbye!');
+    }
+
+    static printCount() {
+        console.log(`${this.counter} instances have been created`);
+    }
+
+    static clone(person) {
+        {
+            const result = new Person(person.firstName, person.lastName, person.age, person.email);
+            return result;
+        }
+    }
+}
+Person.printCount();
 const myPerson = new Person('John', 'Smith', 32, 'john@abv.bg');
 console.log(myPerson.toString());
+
+Person.greet();
+// myPerson.greet(); // static method is part of the class not of each instance
+Person.printCount();
+
+const myClone = Person.clone(myPerson);
+console.log(myPerson);
+console.log(myClone);
+console.log(myPerson == myClone);
---
+++ JS/PA January 2024 JS/17. Classes/Point.js
@@ -0,0 +1,14 @@
+class Point {
+    x;
+    y;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    static distance(p1, p2) {
+        return 
+    }
+}
