dc06de7c8c6eab18653c5ebdae6836d6f3e283fc
Public Name <137268124+StPKr@users.noreply.github.com>
2024-02-03 17:21:44 +0200

    Update Circle.js

--- JS/PA January 2024 JS/17. Classes/Circle.js
+++ JS/PA January 2024 JS/17. Classes/Circle.js
@@ -5,8 +5,29 @@ class Circle {
     constructor(r) {
         this.radius = r;
     }
+
+    get area() {
+        return Math.PI * (this.radius ** 2);
+    }
+
+    get diameter() {
+        return 2 * this.radius;
+    }
+    set diameter(value) {
+        this.radius = value / 2;
+    }
 }
 
 const myCircle = new Circle(5);
 console.log(myCircle);
+console.log(myCircle.radius);
+console.log(myCircle.area);
+console.log(myCircle.diameter);
+
+myCircle.radius = 3
+console.log(myCircle.area);
+console.log(myCircle.diameter);
+
+myCircle.diameter = 4;
+
 console.log(myCircle.radius);
