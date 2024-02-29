327d6144a91b7e1f2e2525557f65cb28ca7703af
Public Name <137268124+StPKr@users.noreply.github.com>
2024-02-29 21:08:21 +0200

    Update app.js

--- JS/04. PA Feb 2024 JS/06. Async Prog Exe/02.Bus-Schedule/app.js
+++ JS/04. PA Feb 2024 JS/06. Async Prog Exe/02.Bus-Schedule/app.js
@@ -1,11 +1,38 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const infoSpanRef = document.querySelector('#info span');
+    const departBtnRef = document.getElementById('depart');
+    const arriveBtnRef = document.getElementById('arrive');
+
+    const url = `http://localhost:3030/jsonstore/bus/schedule/`;
+    const stop = {
+        currentStop: "",
+        nextStop: "depot"
+    }
+
+    async function depart() {
+
+        try {
+            const response = await fetch(url + stop.nextStop);
+            const data = await response.json();
+            stop.currentStop = data.name;
+            stop.nextStop = data.next;
+            infoSpanRef.textContent = `Next stop ${stop.currentStop}`;
+            departBtnRef.disabled = true;
+            arriveBtnRef.disabled = false;
+        } catch (err) {
+            infoSpanRef.textContent = "Error";
+            departBtnRef.disabled = true;
+            arriveBtnRef.disabled = true;
+        }
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoSpanRef.textContent = `Arriving at ${stop.currentStop}`;
+        departBtnRef.disabled = false;
+        arriveBtnRef.disabled = true;
+
     }
 
     return {
