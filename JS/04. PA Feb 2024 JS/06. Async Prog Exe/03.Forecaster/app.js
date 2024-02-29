0dd4abdc7be0b41dff3ab3b47b32955f5a072405
Public Name <137268124+StPKr@users.noreply.github.com>
2024-02-29 22:31:18 +0200

    Task 2

--- JS/04. PA Feb 2024 JS/06. Async Prog Exe/03.Forecaster/app.js
+++ JS/04. PA Feb 2024 JS/06. Async Prog Exe/03.Forecaster/app.js
@@ -1,5 +1,107 @@
 function attachEvents() {
-    console.log("TODO...");
+    let submitBtn = document.getElementById('submit');
+    submitBtn.addEventListener('click', getWeather);
+
+    const forecastSection = document.getElementById('forecast');
+    const locationInput = document.getElementById('location');
+    const currentSection = document.getElementById('current');
+    const upcomingSection = document.getElementById('upcoming');
+
+    const baseLocationUrl = `http://localhost:3030/jsonstore/forecaster/locations`;
+    const todayUrl = `http://localhost:3030/jsonstore/forecaster/today/`;
+    const upcomingUrl = `http://localhost:3030/jsonstore/forecaster/upcoming/`
+
+    async function getWeather() {
+        try {
+            const userInput = locationInput.value;
+            forecastSection.style.display = 'block';
+            const locationResponse = await fetch(baseLocationUrl);
+            const locationData = await locationResponse.json();
+            const currentLocationData = locationData.find(x => x.name == userInput);
+            await fillTodayData(currentLocationData.code);
+            await fillUpcomingData(currentLocationData.code);
+        } catch (e) {
+            forecastSection.textContent = "Error";
+        }
+
+    }
+
+    async function fillTodayData(code) {
+        const response = await fetch(todayUrl + code);
+        const data = await response.json();
+        const todayInfo = createForcastTodaySection(data);
+        currentSection.appendChild(todayInfo);
+    }
+
+    async function fillUpcomingData(code) {
+        const response = await fetch(upcomingUrl + code);
+        const data = await response.json();
+        const upcomingInfo = createForecastUpcomingSection(data);
+        upcomingSection.appendChild(upcomingInfo);
+    }
+
+    function createForecastUpcomingSection(data) {
+        const container = document.createElement('div');
+        container.classList.add('forecast-info');
+        const upcoming1 = generateSpan('upcoming', data.name, data.forecast[0]);
+        const upcoming2 = generateSpan('upcoming', data.name, data.forecast[1]);
+        const upcoming3 = generateSpan('upcoming', data.name, data.forecast[2]);
+        container.appendChild(upcoming1);
+        container.appendChild(upcoming2);
+        container.appendChild(upcoming3);
+
+        return container;
+    }
+
+    function generateSpan(classes, name, data) {
+        const spanContainer = document.createElement('span');
+        spanContainer.classList.add(classes);
+
+        const spanName = document.createElement('span');
+        spanName.classList.add('forecast-data');
+        spanName.textContent = name
+
+        const degree = document.createElement('span');
+        degree.classList.add('forecast-data');
+        degree.innerHTML = `${data.low + findSymbol("Degrees")}/${data.high + findSymbol("Degrees")}`;
+
+        const condition = document.createElement('span');
+        condition.classList.add('forecast-data');
+        condition.textContent = data.condition;
+
+        spanContainer.appendChild(spanName);
+        spanContainer.appendChild(degree);
+        spanContainer.appendChild(condition);
+
+        return spanContainer;
+    }
+
+    function createForcastTodaySection(data) {
+        const container = document.createElement('div');
+        container.classList.add('forecasts');
+        const conditionSpan = document.createElement('span');
+        conditionSpan.classList.add('condition',);
+        conditionSpan.classList.add('symbol');
+        conditionSpan.innerHTML = findSymbol(data.forecast.condition);
+
+        container.appendChild(conditionSpan);
+        const spanContainer = generateSpan('condition', data.name, data.forecast);
+
+        container.appendChild(spanContainer);
+        return container;
+    }
+
+    function findSymbol(condition) {
+        switch (condition) {
+            case "Sunny": return '&#x2600';
+            case "Partly sunny": return '&#x26C5';
+            case "Overcast": return '&#x2614';
+            case "Rain": return '&#x2600';
+            case "Degrees": return '&#176';
+
+        }
+    }
+
 }
 
 attachEvents();
