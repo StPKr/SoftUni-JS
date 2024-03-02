15f19e1f88f424c33c664b8a7045cff4af392a77
Public Name <137268124+StPKr@users.noreply.github.com>
2024-03-02 17:53:51 +0200

    Tassk 2

--- JS/04. PA Feb 2024 JS/08. Data and Auth Ex/02.Phonebook/app.js
+++ JS/04. PA Feb 2024 JS/08. Data and Auth Ex/02.Phonebook/app.js
@@ -1,5 +1,62 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+    const ulRef = document.getElementById('phonebook')
+    document.getElementById('btnLoad').addEventListener('click', onLoadAllRecords);
+    document.getElementById('btnCreate').addEventListener('click', onCreateRecord);
+
+    async function onCreateRecord(e) {
+        let personRef = document.getElementById('person');
+        let phoneRef = document.getElementById('phone');
+
+        let person = personRef.value;
+        let phone = phoneRef.value;
+
+        if (!person || !phone) {
+            return;
+        }
+
+        let data = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ person, phone })
+        }
+
+        await fetch(url, data);
+        personRef.value = "";
+        phoneRef.value = "";
+        onLoadAllRecords();
+    }
+
+    async function onLoadAllRecords(e) {
+        let response = await fetch(url);
+        let data = await response.json();
+        ulRef.innerHTML = "";
+        Object.values(data).forEach(rec => {
+            createAndAppendLi(rec);
+        });
+    }
+
+    function createAndAppendLi(data) {
+        let li = document.createElement('li');
+        li.textContent = `${data.person}: ${data.phone}`;
+        let btn = document.createElement('button');
+        btn.textContent = "Delelte";
+        btn.dataset.id = data._id;
+        btn.addEventListener('click', onDelete);
+
+        li.appendChild(btn);
+        ulRef.appendChild(li);
+    }
+
+    async function onDelete(e) {
+        let id = e.target.dataset.id;
+        await fetch(url + "/" + id, {
+            method: "DELETE"
+        });
+        onLoadAllRecords();
+    }
 }
 
 attachEvents();
