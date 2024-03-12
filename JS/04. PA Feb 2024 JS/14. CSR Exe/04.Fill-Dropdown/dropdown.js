6a0dceb42143b5dc2cbf165192d7a09ec770ece9
Public Name <137268124+StPKr@users.noreply.github.com>
2024-03-12 21:04:09 +0200

    Task 4

--- JS/04. PA Feb 2024 JS/14. CSR Exe/04.Fill-Dropdown/dropdown.js
+++ JS/04. PA Feb 2024 JS/14. CSR Exe/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,41 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from './node_modules/lit-html/lit-html.js';
+
+const root = document.getElementById('menu');
+document.querySelector('form').addEventListener('submit', addItem);
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown';
+onLoad();
+
+async function onLoad() {
+    const response = await fetch(url);
+    const data = await response.json();
+    const option = Object.values(data).map(op => optionTemp(op));
+    update(option);
+}
+
+function optionTemp(data) {
+    return html`<option value=${data._id}>${data.text}</option>`
+}
+
+function update(data) {
+    render(data, root)
+
+}
+
+function addItem(e) {
+    e.preventDefault();
+    const inputRef = document.getElementById('itemText');
+    const text = inputRef.value;
+    inputRef.value = '';
+    addItemInDb({ text });
+}
+
+async function addItemInDb(data) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    onLoad();
 }
