fda97898e76c9f0eddbd21f2a2e80808f64db799
Public Name <137268124+StPKr@users.noreply.github.com>
2024-03-12 20:38:25 +0200

    Task 3

--- JS/04. PA Feb 2024 JS/14. CSR Exe/03.Search-in-List/search.js
+++ JS/04. PA Feb 2024 JS/14. CSR Exe/03.Search-in-List/search.js
@@ -1,3 +1,33 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js'
+
+document.querySelector('button').addEventListener('click', search);
+
+const townsRoot = document.getElementById('towns');
+const resultRoot = document.getElementById('result');
+const inputRef = document.getElementById('searchText');
+
+update();
+
+function update(match) {
+   render(ulTemplate(towns, match), townsRoot)
+}
+
+function ulTemplate(towns, match) {
+   return html`<ul>${towns.map(town => createLiTemplate(town, match?.includes(town)))}</ul>`
+}
+
+function createLiTemplate(town, isActive) {
+   return html`<li class=${isActive ? 'active' : ''}>${town}</li>`
+}
+
 function search() {
-   // TODO
+   const searchText = inputRef.value;
+   const match = towns.filter(town => town.includes(searchText));
+   update(match);
+   renderMatchcount(match.length);
+}
+
+function renderMatchcount(count) {
+   render(html`${count} matches found`, resultRoot)
 }
