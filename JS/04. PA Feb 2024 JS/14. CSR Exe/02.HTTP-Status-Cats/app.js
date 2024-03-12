9dfba195c000ff31945ee722bb748c033d430769
Public Name <137268124+StPKr@users.noreply.github.com>
2024-03-12 20:10:58 +0200

    update

--- JS/04. PA Feb 2024 JS/14. CSR Exe/02.HTTP-Status-Cats/app.js
+++ JS/04. PA Feb 2024 JS/14. CSR Exe/02.HTTP-Status-Cats/app.js
@@ -19,7 +19,7 @@ function createCatcard(cat) {
         <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
          <div class="info">
             <button @click=${toggleCatStatus} class="showBtn">Show status code</button>
-            <div class="status" style="display: none" id="100">
+            <div class="status" style="display: none" id=${cat.id}>
                 <h4>Status Code: ${cat.statusCode}</h4>
               <p>${cat.statusMessage}</p>
              </div>
