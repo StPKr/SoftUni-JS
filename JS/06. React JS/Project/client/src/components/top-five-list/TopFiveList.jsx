c34126e1fe2b5c01e586b90736ddf39016ba2abb
Public Name <137268124+StPKr@users.noreply.github.com>
2024-08-11 10:33:45 +0300

    .

--- JS/06. React JS/Project/client/src/components/top-five-list/TopFiveList.jsx
+++ JS/06. React JS/Project/client/src/components/top-five-list/TopFiveList.jsx
@@ -2,15 +2,24 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import './TopFiveList.css';
 import { booksAPI } from "../../api/books-api";
+import Spinner from "../spinner/Spinner";
 
 export default function TopFiveList() {
     const [pastThreeBooks, setPastThreeBooks] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
 
     useEffect(() => {
         (async () => {
-            const response = await booksAPI.getMostLiked('');
+            try {
+                const response = await booksAPI.getMostLiked('');
 
-            setPastThreeBooks(response)
+                setPastThreeBooks(response)
+            } catch (err) {
+                alert(err.message);
+            } finally {
+                setIsLoading(false);
+            }
         })();
     }, []);
 
@@ -24,41 +33,46 @@ export default function TopFiveList() {
 
     return (
         <>
-            <h1 id="top-five">Top Rated Books</h1>
-            <div id="top-rated-books">
-                <div className="past-discussions">
-                    {pastThreeBooks.map(pastBook => (
-                        <div key={pastBook._id} className='past-book'>
-                            <div className='past-book-banner'>
-                                <Link to={`/books/${pastBook._id}`}>
-                                    <img src={pastBook.cover} alt="banner" />
-                                </Link>
-                            </div>
-                            <div className="past-book-details">
-                                <h2 className='past-week-book-title'>{pastBook.title}
-                                </h2>
-                                <p className='summary'>{truncateSummary(pastBook.summary)}</p>
-                            </div>
-                            <div>
-                                {/* <button className='see-discussion'>See Discussion</button> */}
-                                <a href="#" className="thumbs-up" >
-                                    <span>&#128077;</span>
-                                </a>
-                                <span><b>
-                                    {pastBook.likes}
-                                </b></span>
-                                <a href="#" className="thumbs-down" >
-                                    <span>&#128078;</span>
-                                </a>
-                                <span><b>
-                                    {pastBook.dislikes}
-                                </b></span>
-                            </div>
-                        </div>
-                    ))}
+            {isLoading
+                ? <Spinner />
+                : <>
+                    <h1 id="top-five">Top Rated Books</h1>
+                    <div id="top-rated-books">
+                        <div className="past-discussions">
+                            {pastThreeBooks.map(pastBook => (
+                                <div key={pastBook._id} className='past-book'>
+                                    <div className='past-book-banner'>
+                                        <Link to={`/books/${pastBook._id}`}>
+                                            <img src={pastBook.cover} alt="banner" />
+                                        </Link>
+                                    </div>
+                                    <div className="past-book-details">
+                                        <h2 className='past-week-book-title'>{pastBook.title}
+                                        </h2>
+                                        <p className='summary'>{truncateSummary(pastBook.summary)}</p>
+                                    </div>
+                                    <div>
+                                        {/* <button className='see-discussion'>See Discussion</button> */}
+                                        <a href="#" className="thumbs-up" >
+                                            <span>&#128077;</span>
+                                        </a>
+                                        <span><b>
+                                            {pastBook.likes}
+                                        </b></span>
+                                        <a href="#" className="thumbs-down" >
+                                            <span>&#128078;</span>
+                                        </a>
+                                        <span><b>
+                                            {pastBook.dislikes}
+                                        </b></span>
+                                    </div>
+                                </div>
+                            ))}
 
-                </div>
-            </div>
+                        </div>
+                    </div>
+                </>
+            }
         </>
     );
 }
