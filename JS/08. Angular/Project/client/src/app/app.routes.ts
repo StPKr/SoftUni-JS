59af95c70945ec0fe4e2b6f1a376c885439019dc
St PKr <sotfuni25@gmail.com>
2024-12-01 10:57:02 +0200

    login form styling

--- JS/08. Angular/Project/client/src/app/app.routes.ts
+++ JS/08. Angular/Project/client/src/app/app.routes.ts
@@ -2,14 +2,21 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TestComponent } from './test/test.component';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { LoginComponent } from './user/login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
 
+    //User
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: LoginComponent },
+
+    //User end
+    
     //*Test component
     { path: 'test', component: TestComponent },
-    //*Test component
+    //*Test component end
 
     { path: '404', component: PageNotFoundComponent },
     { path: '**', redirectTo: '/404' },
