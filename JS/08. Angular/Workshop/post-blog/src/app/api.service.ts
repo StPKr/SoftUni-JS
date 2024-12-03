1f1cc2be8e4af229f8e5c1d7654c879cb88ae17a
St PKr <sotfuni25@gmail.com>
2024-12-03 21:26:53 +0200

    minor corrections

--- JS/08. Angular/Workshop/post-blog/src/app/api.service.ts
+++ JS/08. Angular/Workshop/post-blog/src/app/api.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment.development';
 import { Post } from './types/post';
 import { Theme } from './types/theme';
 
