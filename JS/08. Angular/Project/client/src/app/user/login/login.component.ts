762adb55d66317d4a52fd401592e19a2f2b6a312
St PKr <sotfuni25@gmail.com>
2024-12-06 18:15:07 +0200

    cosmetic changes

--- JS/08. Angular/Project/client/src/app/user/login/login.component.ts
+++ JS/08. Angular/Project/client/src/app/user/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AutoFocusDirective } from '../../directives/auto-focus.directive';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [AutoFocusDirective],
+  imports: [AutoFocusDirective, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- JS/08. Angular/Project/client/src/app/user/register/register.component.ts
+++ JS/08. Angular/Project/client/src/app/user/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AutoFocusDirective } from '../../directives/auto-focus.directive';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [AutoFocusDirective],
+  imports: [AutoFocusDirective, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
