d0684507197c03b24f999d11e0904c5c061de822
St PKr <sotfuni25@gmail.com>
2024-12-07 16:15:33 +0200

    login Form creation

--- JS/08. Angular/Project/client/src/app/user/login/login.component.ts
+++ JS/08. Angular/Project/client/src/app/user/login/login.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { AutoFocusDirective } from '../../directives/auto-focus.directive';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [AutoFocusDirective, RouterLink],
+  imports: [AutoFocusDirective, RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor(private userService: UserService, private router: Router) { }
+
+  login(form: NgForm) {
+    if (form.invalid) {
+      console.log('Invalidd login form');
+      return;
+    }
+
+    const { email, password } = form.value;
+
+    this.userService.login(email, password).subscribe(() => {
+      this.router.navigate(['/home']);
+    });
+  }
 }
